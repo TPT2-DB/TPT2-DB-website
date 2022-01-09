d2bbb84b61bfef1df84a39a2987a166f6bb9c741
VL07 <79533745+VL07@users.noreply.github.com>
2022-01-09 19:15:51 +0100

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,8 @@
 import React from 'react'
 
 export default function Header() {
+	const { data: session } = useSession()
+	
 	return (
 		<div className="header w-screen h-auto bg-blue-500 drop-shadow-sm fixed flex">
 			
