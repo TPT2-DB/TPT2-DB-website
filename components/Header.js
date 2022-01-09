72d6291d2847ee9f6a53b500d542d348acae757b
VL07 <79533745+VL07@users.noreply.github.com>
2022-01-09 18:56:31 +0100

    Added tailwind

---
+++ components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header() {
+	return (
+		<div className="header w-screen h-auto bg-blue-500 drop-shadow-sm fixed flex">
+			
+		</div>
+	)
+}
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,16 @@
 import { SessionProvider } from "next-auth/react"
+import Header from "../components/Header"
+import "../styles/globals.css"
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }) {
+  console.log(session)
+  
   return (
     <SessionProvider session={session}>
+      <Header />
       <Component {...pageProps} />
     </SessionProvider>
   )
