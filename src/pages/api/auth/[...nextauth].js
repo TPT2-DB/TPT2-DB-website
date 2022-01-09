aa15466d4088b1b70daae8b273d1867649bda20f
VL07 <79533745+VL07@users.noreply.github.com>
2022-01-09 12:57:57 +0100

    Added a way to sign in

---
+++ src/pages/api/auth/[...nextauth].js
@@ -0,0 +1,16 @@
+import NextAuth from "next-auth"
+import DiscordProvider from "next-auth/providers/discord"
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter"
+import clientPromise from "../../../lib/mongodb"
+
+export default NextAuth({
+    // Configure one or more authentication providers
+    secret: process.env.SECRET,
+    adapter: MongoDBAdapter(clientPromise),
+    providers: [
+        DiscordProvider({
+            clientId: process.env.CLIENT_ID,
+            clientSecret: process.env.CLIENT_SECRET,
+        }),
+    ],
+})
