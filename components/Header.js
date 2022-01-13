1880a402c67e256ec90d56b179aaacb49a957871
VL07 <79533745+VL07@users.noreply.github.com>
2022-01-13 08:53:13 +0100

    Added a link in header logo

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import UserInfo from './UserInfo'
 import { useSession } from "next-auth/react"
+import Link from 'next/link'
 
 export default function Header() {
 	const { data: session } = useSession()
@@ -8,7 +9,9 @@ export default function Header() {
 	return (
 		<div className="header w-screen h-auto bg-blue-500 drop-shadow-xl fixed flex justify-between pl-4 pr-4 pb-2 pt-2 items-center top-0">
 			<div></div>
-			<h1 className="font-bold text-4xl text-white m-0">TPT2 DB</h1>
+			<Link href={"/"}>
+				<h1 className="font-bold text-4xl text-white m-0">TPT2 DB</h1>
+			</Link>
 			<UserInfo session={session} />
 		</div>
 	)
