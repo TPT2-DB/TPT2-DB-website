a8b4f5b2b133f8941f759794959e6436ff75e0e8
VL07 <79533745+VL07@users.noreply.github.com>
2022-01-14 17:38:24 +0100

    Added add roller coaster to api

---
+++ pages/api/rollercoaster/add.js
@@ -0,0 +1,41 @@
+import { getSession } from "next-auth/react"
+import PlayerModel from "../../../lib/schemas/Player"
+import RollerCoasterModel from "../../../lib/schemas/RollerCoaster"
+
+export default async (req, res) => {
+	const session = await getSession({ req })
+
+	if (session) {
+		try {
+			const userDoc = await PlayerModel.findById(session.id)
+
+  			const {name, description, content} = req.query
+
+			if (!name || !description || !content) {
+				res.status(400).json({error: "missing parameters"}).end()
+				return
+			}
+
+			const rollerCoasterDoc = new RollerCoasterModel({
+				name,
+				description,
+				content: content,
+				created: Date.now(),
+				creator: session.id
+			})
+			rollerCoasterDoc.save()
+
+			userDoc.rollerCoasters.push(rollerCoasterDoc._id)
+			userDoc.save()
+
+			res.status(201).json(rollerCoasterDoc)
+			
+		} catch (err) {
+			console.error(err)
+			res.status(500)
+		}
+	} else {
+		res.status(401)
+	}
+	res.end()
+}
